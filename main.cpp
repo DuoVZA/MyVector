e152002866ab03781da9c3bf164b51b8cd97a918
DuoVZA <zayvlad2009@gmail.com>
2025-10-15 11:54:43 +0300

    hi

--- main.cpp
+++ main.cpp
@@ -9,27 +9,6 @@ int main()
 	MyVector vect1(10);
 	vect1.Init();
 	vect1.Print();
-	MyVector vect2 = vect1;
 	vect2.Print();
-	MyVector vect3 = move(vect2);
-	vect3.Print();
-
-	for (int i = 0; i < vect1.GetSize(); i++)
-	{
-		cout << vect1[i] << "\t";
-	}
-	cout << endl;
-
-	MyVector vect4 = ++vect3;
-	vect4.Print();
-	MyVector vect5 = --vect4;
-	vect4.Print();
-	vect5.Print();
-
-	vect5 /= 20;
-
-	int size = vect5;
-	cout << size << endl;
-
-	vect5.Print();
+	//+=, -=, *=, /=
 }
